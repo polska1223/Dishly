f715c469aba7a466ab805aaab86ef128330a1658
MD-DD <Mikolajdanicki34@gmail.com>
2026-05-27 14:24:16 +0200

    supabase connectie en routes

--- dishly/src/App.jsx
+++ dishly/src/App.jsx
@@ -1,10 +1,14 @@
 import { Routes, Route } from "react-router-dom";
-
+import { supabase } from "./supabase";
+import Auth from "./pages/Auth";
+import PrivateRoute from "./components/PrivateRoute";
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<h1>Home</h1>} />
+      <Route path="/" element={<PrivateRoute> <h1>Home</h1></PrivateRoute>} />
       <Route path="/test" element={<h1>Test pagina</h1>} />
+      <Route path="/login" element={<Auth mode="login" />} />
+      <Route path="/register" element={<Auth mode="register" />} />
     </Routes>
   );
 }
