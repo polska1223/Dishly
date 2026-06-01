a5fc6eefc86faf5c4484a701c18a092f5ec90b6b
MD-DD <Mikolajdanicki34@gmail.com>
2026-06-01 11:37:48 +0200

    fout melding verwijderen en hopleijk goed pushen

--- dishly/src/App.jsx
+++ dishly/src/App.jsx
@@ -1,5 +1,4 @@
 import { Routes, Route } from "react-router-dom";
-<<<<<<< HEAD
 import Auth from "./pages/Auth";
 import Home from "./pages/Home";
 import PrivateRoute from "./components/PrivateRoute";
@@ -20,25 +19,4 @@ export default function App() {
             <Route path="/register" element={<Auth mode="register" />} />
         </Routes>
     );
-=======
-import { supabase } from "./supabase";
-import Auth from "./pages/Auth";
-import PrivateRoute from "./components/PrivateRoute";
-import { useSession } from "../hooks/useSession";
-export default function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<PrivateRoute> <h1>Home</h1></PrivateRoute>} />
-      <Route path="/test" element={<h1>Test pagina</h1>} />
-      <Route path="/login" element={<Auth mode="login" />} />
-      <Route path="/register" element={<Auth mode="register" />} />
-    </Routes>
-  );
-
-  const { session} = useSession();
-
-
-  <p>{session?.email}</p>
-
->>>>>>> f8dd5f4b1d21724e48999afec383d7e43615d602
 }
